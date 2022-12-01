653448901b6c69ae167f9220e67d472db32721d6
juanpablo <juan2224649@correo.uis.edu.co>
2022-12-01 10:14:04 -0500

    Se agrega interfaz del programa con sus funciones pero aun no hace la matriz

--- Matriz_parcial.py
+++ Matriz_parcial.py
@@ -1,64 +1,100 @@
-#Se importa libreria tkinter con todas sus funciones
-import matplotlib.pyplot as plt
+
 from tkinter import *
-from tkinter import messagebox
-import lineal
-import numpy as npp
-#-------------------
-#funciones de la app
-#-------------------
+import random
 
 
-ventana_principal = Tk()
-ventana_principal.title("Menu de funciones")
-ventana_principal.geometry("550x300")
-ventana_principal.minsize(300, 150)
-
-# Color de la ventana
-ventana_principal.config(bg="black")
-
-frame_opciones = Frame(ventana_principal)
-frame_opciones.config(bg = "green", width = 528 , height = 270)
-frame_opciones.place(x = 10, y = 15)
+root = Tk()
+root.title("Buscador de numeros en una matriz")
+root.geometry("800x600")
+root.configure(background = "light blue")
 
 
+etiqueta =Label(root, text = "Ingrese el numero de filas y columnas de la matriz:")
+
+#se posiciona la etiqueta en la ventana
+etiqueta.pack()
+
+#se crea una caja de texto para ingresar el numero de filas y columnas
+caja_texto = Entry(root)
 
 
-# Label
-menu = Label(frame_opciones, text = "Bienvenido a la graficadora de funciones")
-menu.config(bg = "green", fg = "white", font = ("Bahnschrift Condensed",20))
-menu.place(x = 95, y = 10)
-
-texto = Label(frame_opciones, text = "Elija una opcion: ")
-texto.config(bg = "green", fg = "white", font = ("Bahnschrift Condensed",20))
-texto.place(x = 36, y = 60)
+#se posiciona la caja de texto en la ventana
+caja_texto.pack()
 
 
-
-#Bonotones de la ventana principal
-boton = Button(bg = "white",text="    Funcion lineal    ",fg = "black",command=lineal.funcion_lineal, font = ("Bahnschrift Condensed",14))
-boton.place(x=115, y=150, anchor="center")
-
-boton = Button(bg = "white",text="Funcion cuadratica",fg = "black",command=lineal.funcion_lineal, font = ("Bahnschrift Condensed",14))
-boton.place(x=115, y=200, anchor="center")
-
-boton = Button(bg = "white",text="   Funcion cubica   ",fg = "black",command=lineal.funcion_lineal, font = ("Bahnschrift Condensed",14))
-boton.place(x=115, y=250, anchor="center")
-
-boton = Button(bg = "white",text="  Funcion exponencial   ",fg = "black",command=lineal.funcion_lineal, font = ("Bahnschrift Condensed",14))
-boton.place(x=280, y=149, anchor="center")
-
-boton = Button(bg = "white",text="   Funcion logaritmica  ",fg = "black",command=lineal.funcion_lineal, font = ("Bahnschrift Condensed",14))
-boton.place(x=280, y=200, anchor="center")
-
-boton = Button(bg = "white",text="Funcion trigonometrica",fg = "black",command=lineal.funcion_lineal, font = ("Bahnschrift Condensed",14))
-boton.place(x=280, y=250, anchor="center")
-
-boton = Button(bg = "white",text="Funcion racionales",fg = "black",command=lineal.funcion_lineal, font = ("Bahnschrift Condensed",14))
-boton.place(x=450, y=173, anchor="center")
-
-boton = Button(bg = "white",text="  Funcion radical   ",fg = "black",command=lineal.funcion_lineal, font = ("Bahnschrift Condensed",14))
-boton.place(x=450, y=225, anchor="center")
+#se define una funcion para generar la matriz
+def generar_matriz():
+    
+    #se obtiene el numero de filas y columnas de la matriz
+    filas_columnas = int(caja_texto.get())
+    
+    #se genera la matriz con numeros aleatorios en un rango determinado
+    matriz = [[random.randint(0,9) for i in range(filas_columnas)] for j in range(filas_columnas)]
+    
+    return matriz
 
 
-ventana_principal.mainloop()
+#se define una funcion para buscar un numero en la matriz
+def buscar_numero(matriz, numero):
+    
+    #se recorre la matriz
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            
+            #si el numero se encuentra en la matriz, se retorna la posicion en la que se encuentra
+            if(matriz[i][j] == numero):
+                return i, j
+    
+    #si el numero no se encuentra en la matriz, se retorna None
+    return None
+        
+
+#se define una funcion para dibujar la matriz
+def dibujar_matriz(matriz):
+    
+    #se obtiene el canvas en el que se va a dibujar la matriz
+    canvas = Canvas(root, width = 600, height = 600)
+    
+    #se obtiene el ancho y el alto de cada elemento de la matriz
+    ancho = 600/len(matriz)
+    alto = 600/len(matriz)
+    
+    #se recorre la matriz
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            
+            #para cada elemento de la matriz, se dibuja un rectangulo en el canvas, de un color determinado
+            canvas.create_rectangle(j*ancho, i*alto, (j+1)*ancho, (i+1)*alto, fill = "red")
+            
+            #dentro de cada rectangulo, se muestra el elemento de la matriz correspondiente
+            canvas.create_text(j*ancho + ancho/2, i*alto + alto/2, text = str(matriz[i][j]), font = ("Arial", 16))
+    
+    #se posiciona el canvas en la ventana
+    canvas.pack()
+    
+    
+    #se obtiene el numero a buscar en la matriz
+    numero = int(input("Ingrese el numero a buscar en la matriz: "))
+    
+    #se llama a la funcion para buscar el numero en la matriz
+    posicion = buscar_numero(matriz, numero)
+    
+    #si el numero se encuentra en la matriz, se dibuja un rectangulo en el canvas, de un color verde, que indica la posicion del numero en la matriz
+    if(posicion != None):
+        canvas.create_rectangle(posicion[1]*ancho, posicion[0]*alto, (posicion[1]+1)*ancho, (posicion[0]+1)*alto, fill = "green")
+    
+    #se muestra un mensaje en la consola, indicando si el numero se encuentra o no en la matriz
+    if(posicion == None):
+        print("El numero no se encuentra en la matriz")
+    else:
+        print("El numero se encuentra en la posicion: ", posicion[0], posicion[1])
+
+
+#se crea un boton para llamar a la funcion que genera la matriz
+boton = Button(root, text = "Generar matriz", command = generar_matriz)
+
+#se posiciona el boton en la ventana
+boton.pack()
+
+
+root.mainloop()
